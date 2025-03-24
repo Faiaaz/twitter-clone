2fe8a97bb9e7ef10640f4bc0d19cd94576f8ecb8
Fahim Faiaz <fahim7faiaz@gmail.com>
2025-03-24 08:25:40 +0600

    updated models

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from .extensions import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    content = db.Column(db.String(280), nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    user = db.relationship("User", backref="tweets")
